93923fa42605680491eebeabf7ac00c2c71dfc00
iforsmann <iforsmann@frontier.com>
2023-03-23 13:42:01 -0700

    foo

--- Final/phenoms.py
+++ Final/phenoms.py
@@ -1,7 +1,24 @@
+import db
+###### potential phenoms #########
+# http://ai4reporters.org/CA_201720180AB447_96_54363_53047
+# bill has no opposition (no no votes)
+# aye <name>: aye or nay
+# measure passes
+# reader stutters, seems to have trouble reading
+# Some summaries are much longer than others
+# "shouldn't even be having this discussion"
+# extreme rhetoric (example from diabetes)
+# "common sense bill"
+# SOME bills gain much more support than others
+# calling absent voters multiple times
 
+# senator yields time to another to introduce bill
+# move to table the bill
+# laughter
 
 def main():
-    pass
+    db.init()
+    print(db.getDiscussion(7))
 
 if __name__ == "__main__":
     main()
